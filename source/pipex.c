7007b060b2d3a56f1e45244ffd799f98efddba1d
AOhapkin <alexeyyago@gmail.com>
2022-02-18 19:01:56 +0300

    add open_outfile()

--- source/pipex.c
+++ source/pipex.c
@@ -8,6 +8,16 @@ int	open_infile(char *file_name)
 	return (open(file_name, O_RDONLY));
 }
 
+int	open_outfile(char *file_name)
+{
+	if (access(file_name, F_OK) < 0)
+	{
+		write(2, "File not found\n", 15);
+		return (0);
+	}
+	return (open(file, O_CREAT | O_WRONLY | O_TRUNC));
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	int	fd[2];
