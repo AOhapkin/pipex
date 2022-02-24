0c807ef654463e798b1bb0eba77870e4054d95f3
Alexey <alexeyyago@gmail.com>
2022-02-24 15:33:30 +0300

    добавил run_command

---
+++ source/run_command.c
@@ -0,0 +1,29 @@
+#include "pipex.h"
+
+char *find_command_path(char *command, char **envp)
+{
+
+}
+
+void    run_command(char *argv, char **envp)
+{
+    char    **command;
+    char    *command_path;
+    int     i;
+
+    command = ft_split(argv, SPACE);
+    command_path = find_command_path(command[0], envp);
+    i = 0;
+    if (!command_path)
+    {
+        while (command[i])
+        {
+            free(command[i])
+            i++;
+        }
+        free(command);
+        error_exit();
+    }
+    if (execve(command_path, command, envp) == -1)
+        error_exit();
+}
